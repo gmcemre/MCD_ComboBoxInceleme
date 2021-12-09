a309381b43abad345a7d18d684b24132ae036c33
emre gemici <gmcc.emree@gmail.com>
2021-12-09 20:03:49 +0300

    code changed

--- MCD_ListBoxInceleme/urunSecimKullanici.Designer.cs
+++ MCD_ListBoxInceleme/urunSecimKullanici.Designer.cs
@@ -29,19 +29,60 @@ namespace MCD_ListBoxInceleme
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(34, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(891, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birden fazla ürün seçimi yaptınız.Hangi ürünün detaylarını görüntülemek istiyorsu" +
+    "nuz?";
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(419, 120);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(120, 57);
+            this.btnKapat.TabIndex = 1;
+            this.btnKapat.Text = "X";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(77, 213);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(811, 364);
+            this.flowLayoutPanel1.TabIndex = 2;
             // 
             // urunSecimKullanici
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 640);
+            this.ClientSize = new System.Drawing.Size(975, 640);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.label1);
             this.Name = "urunSecimKullanici";
             this.Text = "urunSecimKullanici";
+            this.Load += new System.EventHandler(this.urunSecimKullanici_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- MCD_ListBoxInceleme/urunSecimKullanici.cs
+++ MCD_ListBoxInceleme/urunSecimKullanici.cs
@@ -12,9 +12,46 @@ namespace MCD_ListBoxInceleme
 {
     public partial class urunSecimKullanici : Form
     {
-        public urunSecimKullanici()
+        urun[] urunListe;
+        public urunSecimKullanici(urun[] secilenUrunler)
         {
             InitializeComponent();
+            urunListe = secilenUrunler;
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void urunSecimKullanici_Load(object sender, EventArgs e)
+        {
+            foreach (var item in urunListe)
+            {
+                Button btn = new Button();
+                btn.Text = item.urunAdi;
+                btn.Name = "btn" + item.id.ToString();
+                btn.Size = new System.Drawing.Size(122, 37);
+                btn.Tag = item;
+                btn.Click += Btn_Click;
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            urun u1 = btn.Tag as urun;
+            Form F1 = Application.OpenForms["Form1"];
+            GroupBox F1GBOX = (GroupBox)F1.Controls["groupBox1"];
+            ((PictureBox)F1.Controls["pctUrunRessim"]).Image = Image.FromFile(u1.urunResim);
+            ((TextBox)F1.Controls["txtUrunAdi"]).Text = u1.urunAdi;
+            ((TextBox)F1.Controls["txtKategori"]).Text = u1.urunKategori;
+            ((TextBox)F1.Controls["txtStokAdet"]).Text = u1.stokAdet.ToString();
+            ((TextBox)F1.Controls["txtYazar"]).Text = u1.yazar;
+            ((TextBox)F1.Controls["txtUrunAciklama"]).Text = u1.aciklama;
+            this.Close();
+
         }
     }
 }
