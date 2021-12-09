2a1ae5096827de34bc6a713b0e41f4b9180ad2b9
emre gemici <gmcc.emree@gmail.com>
2021-12-09 18:13:38 +0300

    Add project files.

---
+++ MCD_ComboBoxInceleme/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace MCD_ComboBoxInceleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbUrunListe = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pctUrunResim = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.txtStokAdet = new System.Windows.Forms.TextBox();
+            this.txtUrunKategori = new System.Windows.Forms.TextBox();
+            this.txtYazar = new System.Windows.Forms.TextBox();
+            this.txtUrunAciklama = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbUrunListe
+            // 
+            this.cmbUrunListe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbUrunListe.FormattingEnabled = true;
+            this.cmbUrunListe.Location = new System.Drawing.Point(235, 94);
+            this.cmbUrunListe.Name = "cmbUrunListe";
+            this.cmbUrunListe.Size = new System.Drawing.Size(294, 33);
+            this.cmbUrunListe.TabIndex = 0;
+            this.cmbUrunListe.SelectedIndexChanged += new System.EventHandler(this.cmbUrunListe_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(195, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 72);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün Listesi";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUrunAciklama);
+            this.groupBox1.Controls.Add(this.txtYazar);
+            this.groupBox1.Controls.Add(this.txtUrunKategori);
+            this.groupBox1.Controls.Add(this.txtStokAdet);
+            this.groupBox1.Controls.Add(this.txtUrunAdi);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pctUrunResim);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(62, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(688, 332);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seçilen Ürün Detay Bilgisi";
+            // 
+            // pctUrunResim
+            // 
+            this.pctUrunResim.Location = new System.Drawing.Point(30, 31);
+            this.pctUrunResim.Name = "pctUrunResim";
+            this.pctUrunResim.Size = new System.Drawing.Size(195, 261);
+            this.pctUrunResim.TabIndex = 0;
+            this.pctUrunResim.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(274, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ürün Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(274, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Stok Adet:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(274, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ürün Kategorisi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(274, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Açıklama:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(274, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 18);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Yazar:";
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(410, 31);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(243, 28);
+            this.txtUrunAdi.TabIndex = 4;
+            // 
+            // txtStokAdet
+            // 
+            this.txtStokAdet.Location = new System.Drawing.Point(410, 67);
+            this.txtStokAdet.Name = "txtStokAdet";
+            this.txtStokAdet.Size = new System.Drawing.Size(243, 28);
+            this.txtStokAdet.TabIndex = 4;
+            // 
+            // txtUrunKategori
+            // 
+            this.txtUrunKategori.Location = new System.Drawing.Point(410, 108);
+            this.txtUrunKategori.Name = "txtUrunKategori";
+            this.txtUrunKategori.Size = new System.Drawing.Size(243, 28);
+            this.txtUrunKategori.TabIndex = 4;
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.Location = new System.Drawing.Point(410, 145);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(243, 28);
+            this.txtYazar.TabIndex = 4;
+            // 
+            // txtUrunAciklama
+            // 
+            this.txtUrunAciklama.Location = new System.Drawing.Point(410, 184);
+            this.txtUrunAciklama.Multiline = true;
+            this.txtUrunAciklama.Name = "txtUrunAciklama";
+            this.txtUrunAciklama.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtUrunAciklama.Size = new System.Drawing.Size(243, 128);
+            this.txtUrunAciklama.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(817, 569);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbUrunListe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbUrunListe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUrunAciklama;
+        private System.Windows.Forms.TextBox txtYazar;
+        private System.Windows.Forms.TextBox txtUrunKategori;
+        private System.Windows.Forms.TextBox txtStokAdet;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pctUrunResim;
+    }
+}
+
---
+++ MCD_ComboBoxInceleme/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_ComboBoxInceleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Items.Add metodu kullanılarak değerler comboBox eklendi..
+            //ComboBoxDoldurV1();
+            ComboBoxDoldurV2();
+        }
+
+        private void ComboBoxDoldurV2()
+        {
+            cmbUrunListe.DataSource = database.UrunTablo;
+        }
+
+        private void ComboBoxDoldurV1()
+        {
+            foreach (var item in database.UrunTablo  )
+            {
+                cmbUrunListe.Items.Add(item);
+            }
+        }
+
+        private void cmbUrunListe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //1.Yöntem
+            object O1 = cmbUrunListe.SelectedItem;
+            urun U1 = (urun)O1;
+
+            //U1 = cmbUrunListe.SelectedItem == null ? null : cmbUrunListe.SelectedItem as urun;
+
+            //2.Yöntem
+            ComboBox secilenComboBox = sender as ComboBox;
+
+            //1.Yöntem eğer form içerisinde bulunan form elemanına özel bir metot yazıldı ise o zaman ilgili fom elemanını direkt olarak çağırmak daha mantıklıdır.
+
+            //2.Yöntem çalışma zamanında oluşturmuş olduğumuz form elemanlarından hangisine tıklandıldığını veya işlem yapıldığını bilmediğimiz için ve metodumuz birden fazla form elemanında olay metedu olarak tanımlandığı için biz sender üzerinden gelen nesneyi kullanır kullanıcının hangi windows form elemanında işlem yapmış olduğunu anlarız.
+
+            pctUrunResim.Image = Image.FromFile(U1.urunResim);
+            pctUrunResim.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            txtUrunAdi.Text = U1.urunAdi;
+            txtUrunKategori.Text = U1.urunKategori;
+            txtStokAdet.Text = U1.stokAdet.ToString();
+            txtYazar.Text = U1.yazar;
+            txtUrunAciklama.Text = U1.aciklama;
+        }
+    }
+}
